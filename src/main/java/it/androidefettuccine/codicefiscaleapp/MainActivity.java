64ff598371e0a1168913ec27516bfc5e5cb0ceae
jackbonaventura <56650936+jackbonaventura@users.noreply.github.com>
2020-06-10 22:16:26 +0200

    gesu bambino

--- src/main/java/it/androidefettuccine/codicefiscaleapp/MainActivity.java
+++ src/main/java/it/androidefettuccine/codicefiscaleapp/MainActivity.java
@@ -8,7 +8,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	    //jackbonaventurbonjona        
+	   super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
