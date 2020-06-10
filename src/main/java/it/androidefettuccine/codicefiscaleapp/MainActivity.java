ec4a4d7fd41bc7a93110aadbf87925916ad950fe
bonjon <45831678+bonjon@users.noreply.github.com>
2020-06-10 23:48:49 +0200

    prova

--- src/main/java/it/androidefettuccine/codicefiscaleapp/MainActivity.java
+++ src/main/java/it/androidefettuccine/codicefiscaleapp/MainActivity.java
@@ -8,8 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	    //jackbonaventurbonjona        
-	   super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
